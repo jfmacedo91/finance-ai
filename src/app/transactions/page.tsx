42feb46e031864f458a6f01f91e8fd2b3894474a
Jean Macedo <jfmacedo91@gmail.com>
2024-11-10 07:30:38 -0300

    feat: ensure transaction table labels are rendered property

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/lib/prisma";
 
 import { Button } from "@/components/ui/button";
-import { ArrowDownUpIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
 import { DataTable } from "@/components/ui/data-table";
 import { transactionColumns } from "./_columns";
 
@@ -12,8 +12,8 @@ export default async function Transactions() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full">
-          Adicionar transação <ArrowDownUpIcon />
+        <Button className="rounded-full font-bold">
+          Adicionar transação <PlusIcon />
         </Button>
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
