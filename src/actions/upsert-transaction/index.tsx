b8e938332674c55618730a07e36cee2e3615c32a
Jean Macedo <jfmacedo91@gmail.com>
2024-11-12 06:26:23 -0300

    fix: upseart transaction function

--- src/actions/upsert-transaction/index.tsx
+++ src/actions/upsert-transaction/index.tsx
@@ -29,11 +29,11 @@ export const upsertTransaction = async (params: AddTransactionParams) => {
   upsertTransactionSchema.parse(params);
 
   await db.transaction.upsert({
-    where: {
-      id: params.id,
-    },
     update: { ...params, userId },
     create: { ...params, userId },
+    where: {
+      id: params?.id ?? "",
+    },
   });
 
   revalidatePath("/transactions");
